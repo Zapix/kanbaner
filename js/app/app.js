5a22c43e4baf74ab5e76304f8ee265cfad2ca785
Aibulatov Aleksandr <zap.aibulatov@gmail.com>
2015-02-28 17:34:10 +0500

    Init kanbaner project

---
+++ js/app/app.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var KanbanerApp = require('./components/KanbanerApp.react');
+
+
+React.render(
+  <KanbanerApp/>,
+  document.getElementById('app')
+);
---
+++ js/app/components/KanbanerApp.react.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+
+var KanbanerApp = React.createClass({
+  render: function() {
+    return (
+      <div className="row">
+        <h1>Kanbanner application</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = KanbanerApp;
