3974aaf41319583d91c0255963d4c7382b19b1de
Aibulatov Aleksandr <zap.aibulatov@gmail.com>
2015-03-06 17:10:50 +0500

    Merge with 3-repository-list

--- js/app/components/RepositoryList.react.js
+++ js/app/components/RepositoryList.react.js
@@ -2,21 +2,104 @@ var
   React = require( "react" ),
   Link = require("react-router").Link,
 
-  Authentication = require( "../mixins/Authentication" ),
+  UserStore = require( "../stores/UserStore" ),
   AppHeader = require( "./AppHeader.react" ),
-  Breadcrumbs = require( "./Breadcrumbs.react" ) ;
+  Breadcrumbs = require( "./Breadcrumbs.react" ),
+  RepositoryListHead = require( "./RepositoryListHead.react" ),
+  RepositoryListItem = require( "./RepositoryListItem.react" );
 
-  RepositoryList = React.createClass({
-    mixins: [ Authentication ],
+  getRepositoryListState = function() {
+    return {
+      user: UserStore.getUser(),
+      repositories: [
+        {
+          id: 1,
+          name: "Kanbaner",
+          full_name: "zapix/kanbaner",
+          open_issues_count: 12,
+          updated_at: "2011-01-26T19:14:43Z",
+          has_issues: true,
+          owner: {
+            id: 346813,
+            login: "Zapix"
+          }
+        },
+        {
+          id: 4,
+          name: "Oblichi-content",
+          full_name: "oblichi/oblichi-content",
+          open_issues_count: 5,
+          updated_at: "2011-01-26T19:14:43Z",
+          has_issues: true,
+          owner: {
+            id: 43,
+            login: "atorich"
+          }
+        },
+        {
+          id: 8,
+          name: "tmnv1",
+          full_name: "dtmnv/tmnv1",
+          open_issues_count: 10,
+          updadated_at: "2011-01-26T19:14:43Z",
+          has_issues: true,
+          owner: {
+            id: 16,
+            login: "dtmnv"
+          }
+        },
+        {
+          id: 1,
+          name: "django-confirmaction",
+          full_name: "zapix/django-confirmcation",
+          open_issues_count: 0,
+          updated_at: "2011-01-26T19:14:43Z",
+          has_issues: false,
+          owner: {
+            id: 346813,
+            login: "Zapix"
+          }
+        }
+      ]
+    }
+
+  };
+
+    RepositoryList = React.createClass({
+
+    getInitialState: function() {
+      return getRepositoryListState();
+    },
 
     render: function() {
+      console.log( this.state );
+      var user = this.state.user;
+
+      var repositoriesWithIssues = this.state.repositories.filter(
+        function( repository ) {
+          return repository.has_issues;
+        }
+      );
+
+      var repositoryItems = repositoriesWithIssues.map(function( repository ) {
+        return (
+          <RepositoryListItem
+            repository={repository}
+            user={user}
+          />
+        )
+      });
       return (
-        <div className="repository-list">
+        <div className="repository-list-component">
           <AppHeader title="List of repositories"/>
           <Breadcrumbs>
             <span>HOME</span>
             <Link to="user-panel">Repository list</Link>
           </Breadcrumbs>
+          <div className="repository-list app-main">
+            <RepositoryListHead/>
+            {repositoryItems}
+          </div>
         </div>
       );
     }
---
+++ js/app/components/RepositoryListHead.react.js
@@ -0,0 +1,28 @@
+var
+  React = require( "react" );
+
+  RepositoryListHead = React.createClass({
+    render: function() {
+      return (
+        <div className="row">
+          <div className="large-5 medium-4 small-12 columns">
+            <strong>Repository:</strong>
+          </div>
+          <div
+            className="large-3 medium-3 hide-for-small-only columns text-center">
+            <strong>Owner:</strong>
+          </div>
+          <div
+            className="large-2 medium-2 hide-for-small-only columns text-center">
+            <strong>Open issues:</strong>
+          </div>
+          <div
+            className="large-2 medium-3 hide-for-small-only columns text-center">
+            <strong>Updated:</strong>
+          </div>
+        </div>
+      )
+    }
+  });
+
+module.exports = RepositoryListHead;
